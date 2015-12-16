cec7fc357ebb489bacf674ea47d71e57ba21449d
Vivek Gahunia <vgahunia@gmail.com>
2015-12-16 16:37:40 -0500

    dogear slide

--- js/main.js
+++ js/main.js
@@ -2,3 +2,35 @@
 $(window).load(function() {
 	$(".loader").fadeOut("slow");
 })
+
+$(document).ready(function() {
+	$("#dogear_img").hover(function() {
+		$(this).attr("src", "images/dogear_hover.png")
+	})
+	$("#dogear_img").mouseout(function() {
+		$(this).attr("src", "images/dogear1.png")
+	})
+
+	$.Velocity
+		.RegisterEffect("shadowIn", {
+			defaultDuration: 600,
+			calls: [
+				[ { opacity: .9, scale: .8, rotateZ: "-5deg" }, 1.0 ]
+			]
+		})
+
+		.RegisterEffect("shadowBack", {
+			defaultDuration: 400,
+			calls: [
+				[ { rotateZ: "0deg", opacity: 1, scale: 1 } ]
+			]
+		})
+
+		$(".vel-button").mouseover(function() {
+			$(this).velocity("shadowIn");
+		})
+		$(".vel-button").mouseout(function() {
+			$(this).velocity("shadowBack");
+		})
+
+})
