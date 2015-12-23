7c6cc6f0a7cf53d407b5477499d5ed8f30ac9f4d
Vivek Gahunia <vgahunia@gmail.com>
2015-12-22 23:10:40 -0500

    tincan slide menu button

--- js/main.js
+++ js/main.js
@@ -83,4 +83,18 @@ $(document).ready(function() {
 		$(this).css("background-color", "rgb(240, 120, 30)");
 	})
 
+	//TINCAN
+	var items = document.querySelectorAll('.circle a');
+
+	for(var i = 0, l = items.length; i < l; i++) {
+	  items[i].style.left = (50 - 35*Math.cos(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+	  
+	  items[i].style.top = (50 + 35*Math.sin(-0.5 * Math.PI - 2*(1/l)*i*Math.PI)).toFixed(4) + "%";
+	}
+
+	document.querySelector('.menu-button').onclick = function(e) {
+	   e.preventDefault(); 
+	   document.querySelector('.circle').classList.toggle('open');
+	}
+
 })
